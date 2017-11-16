8ff294036159b223ce7a72a60f2a17035f62b130
Nie Mengjun <1107451438@qq.com>
2017-11-16 19:37:02 +0800

    添加常用工具类

--- app/src/main/java/com/tinker/deeson/mytinkerdemo/MainActivity.java
+++ app/src/main/java/com/tinker/deeson/mytinkerdemo/MainActivity.java
@@ -1,27 +1,46 @@
 package com.tinker.deeson.mytinkerdemo;
 
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.debug.hv.ViewServer;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_load).setOnClickListener(this);
-        findViewById(R.id.btn_kill).setOnClickListener(this);
+        ViewServer.get(this).addWindow(this);
+        Log.d("json", "onCreate: " + Utils.isDeviceRooted());
+        Log.d("json", "UDID:" + Utils.getHardDevice());
+
+        Button btnkill = (Button) findViewById(R.id.btn_kill);
+        Button btnload = (Button) findViewById(R.id.btn_load);
+        TextView tvtitle = (TextView) findViewById(R.id.tv_title);
+
+        btnload.setOnClickListener(this);
+        btnkill.setOnClickListener(this);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Utils.setBackground(false);
+        ViewServer.get(this).setFocusedWindow(this);
     }
 
     @Override
@@ -32,29 +51,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_load:
                 loadPatch();
-            break;
+                break;
             case R.id.btn_kill:
                 killApp();
-            break;
+                break;
         }
     }
 
     /**
      * 加载热补丁插件
      */
-    public void loadPatch() {
+    private void loadPatch() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myTinkerDemo/";
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdir();
+        } else {
+            Toast.makeText(this, "该路径已存在！", Toast.LENGTH_SHORT).show();
+        }
         TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
                 Environment.getExternalStorageDirectory().getAbsolutePath() + "/myTinkerDemo/TinkerPatch");
+        Log.d("patch", "path:" + Environment.getExternalStorageDirectory().getAbsolutePath() + "/myTinkerDemo/TinkerPatch");
     }
 
     /**
      * 杀死应用加载补丁
      */
-    public void killApp() {
+    private void killApp() {
         ShareTinkerInternals.killAllOtherProcess(getApplicationContext());
         android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
     }
 }
--- app/src/main/java/com/tinker/deeson/mytinkerdemo/Utils.java
+++ app/src/main/java/com/tinker/deeson/mytinkerdemo/Utils.java
@@ -16,33 +16,43 @@
 
 package com.tinker.deeson.mytinkerdemo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
-
+import android.text.TextUtils;
+import android.util.Log;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
 
-public class Utils {
+class Utils {
 
     /**
      * the error code define by myself
      * should after {@code ShareConstants.ERROR_PATCH_INSERVICE
      */
-    public static final int ERROR_PATCH_GOOGLEPLAY_CHANNEL      = -5;
-    public static final int ERROR_PATCH_ROM_SPACE               = -6;
-    public static final int ERROR_PATCH_MEMORY_LIMIT            = -7;
-    public static final int ERROR_PATCH_ALREADY_APPLY           = -8;
-    public static final int ERROR_PATCH_CRASH_LIMIT             = -9;
-    public static final int ERROR_PATCH_RETRY_COUNT_LIMIT       = -10;
+    private static final int ERROR_PATCH_GOOGLEPLAY_CHANNEL = -5;
+    private static final int ERROR_PATCH_ROM_SPACE = -6;
+    private static final int ERROR_PATCH_MEMORY_LIMIT = -7;
+    public static final int ERROR_PATCH_ALREADY_APPLY = -8;
+    public static final int ERROR_PATCH_CRASH_LIMIT = -9;
+    public static final int ERROR_PATCH_RETRY_COUNT_LIMIT = -10;
     public static final int ERROR_PATCH_CONDITION_NOT_SATISFIED = -11;
 
     public static final String PLATFORM = "platform";
 
-    public static final int MIN_MEMORY_HEAP_SIZE = 45;
+    private static final int MIN_MEMORY_HEAP_SIZE = 45;
 
     private static boolean background = false;
 
@@ -97,10 +107,7 @@ public class Utils {
             allSize = 0;
         }
 
-        if (allSize != 0 && availableSize > limitSize) {
-            return true;
-        }
-        return false;
+        return allSize != 0 && availableSize > limitSize;
     }
 
     public static String getExceptionCauseString(final Throwable ex) {
@@ -151,4 +158,222 @@ public class Utils {
             return src;
         }
     }
+
+    public static boolean checkPermission(Context context, String permission) {
+        boolean result = false;
+        if (Build.VERSION.SDK_INT >= 23) {
+            try {
+                Class<?> clazz = Class.forName("android.content.Context");
+                Method method = clazz.getMethod("checkSelfPermission", String.class);
+                int rest = (Integer) method.invoke(context, permission);
+                if (rest == PackageManager.PERMISSION_GRANTED) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            } catch (Exception e) {
+                result = false;
+            }
+        } else {
+            PackageManager pm = context.getPackageManager();
+            if (pm.checkPermission(permission, context.getPackageName()) == PackageManager.PERMISSION_GRANTED) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static String getUDID(Context context) {
+        try {
+            org.json.JSONObject json = new org.json.JSONObject();
+            android.telephony.TelephonyManager tm = (android.telephony.TelephonyManager) context
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+            String device_id = null;
+            if (checkPermission(context, Manifest.permission.READ_PHONE_STATE)) {
+                device_id = tm.getDeviceId();
+            }
+            String wifiMac = getWifiMac(context);
+            if (!TextUtils.isEmpty(wifiMac) && !"aa-aa-aa-aa-aa-aa".equals(wifiMac)) {
+                json.put("mac", wifiMac);
+            }
+            if (TextUtils.isEmpty(device_id)) {
+                device_id = wifiMac;
+            }
+            if (TextUtils.isEmpty(device_id)) {
+                device_id = android.provider.Settings.Secure.getString(context.getContentResolver(),
+                        android.provider.Settings.Secure.ANDROID_ID);
+            }
+            json.put("device_id", device_id);
+            return json.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static String getWifiMac(Context context) {
+        String macSerial = null;
+        String str = "";
+
+        try {
+            WifiManager wifiMgr = (WifiManager) context.getApplicationContext()
+                    .getSystemService(Context.WIFI_SERVICE);
+            WifiInfo info = (null == wifiMgr ? null : wifiMgr
+                    .getConnectionInfo());
+            if (null != info) {
+                if (!TextUtils.isEmpty(info.getMacAddress())) {
+                    macSerial = info.getMacAddress().replace(":", "-");
+                }
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+        }
+
+        if (macSerial == null || "02:00:00:00:00:00".equals(macSerial)) {
+            try {
+                Process pp = Runtime.getRuntime().exec(
+                        "cat /sys/class/net/wlan0/address ");
+                InputStreamReader ir = new InputStreamReader(pp.getInputStream());
+                LineNumberReader input = new LineNumberReader(ir);
+
+                for (; null != str; ) {
+                    str = input.readLine();
+                    if (str != null) {
+                        macSerial = str.trim();
+                        break;
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (macSerial == null || "02:00:00:00:00:00".equals(macSerial)) {
+            macSerial = "aa-aa-aa-aa-aa-aa";
+        }
+        return macSerial;
+    }
+
+    /**
+     * Return pseudo unique ID
+     *
+     * @return ID
+     */
+    public static String getUniquePsuedoID() {
+        //we make this look like a valid IMEI
+        String m_szDevIDShort = "35" +
+                Build.BOARD.length() % 10 + Build.BRAND.length() % 10 +
+                Build.CPU_ABI.length() % 10 + Build.DEVICE.length() % 10 +
+                Build.DISPLAY.length() % 10 + Build.HOST.length() % 10 +
+                Build.ID.length() % 10 + Build.MANUFACTURER.length() % 10 +
+                Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10 +
+                Build.TAGS.length() % 10 + Build.TYPE.length() % 10 +
+                Build.USER.length() % 10; //13 digits
+        Log.d("json", "m_szDevIDShort:" + m_szDevIDShort);
+        String serial = null;
+        try {
+            serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+            Log.d("json", "serial:" + serial);
+            // Go ahead and return the serial for api => 9
+            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+        } catch (Exception e) {
+            // String needs to be initialized
+            serial = "serial";
+        }
+
+        // Finally, combine the values we have found by using the UUID class to create a unique identifier
+        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+    }
+
+    public static String getHardDevice() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("主板" + Build.BOARD + "\n");
+        sb.append(
+                "系统启动程序版本号：" + Build.BOOTLOADER + "\n");
+        sb.append(
+                "系统定制商：" + Build.BRAND + "\n");
+        sb.append(
+                "cpu指令集：" + Build.CPU_ABI + "\n");
+        sb.append(
+                "cpu指令集2 " + Build.CPU_ABI2 + "\n");
+        sb.append(
+                "设置参数：" + Build.DEVICE + "\n");
+        sb.append(
+                "显示屏参数：" + Build.DISPLAY + "\n");
+        sb.append(
+                "无线电固件版本：" + Build.getRadioVersion() + "\n");
+        sb.append(
+                "硬件识别码:" + Build.FINGERPRINT + "\n");
+        sb.append(
+                "硬件名称：" + Build.HARDWARE + "\n");
+        sb.append(
+                "HOST:" + Build.HOST + "\n");
+        sb.append(
+                "修订版本列表：" + Build.ID + "\n");
+        sb.append(
+                "硬件制造商：" + Build.MANUFACTURER + "\n");
+        sb.append(
+                "版本：" + Build.MODEL + "\n");
+        sb.append(
+                "硬件序列号：" + Build.SERIAL + "\n");
+        sb.append(
+                "手机制造商：" + Build.PRODUCT + "\n");
+        sb.append(
+                "描述Build的标签：" + Build.TAGS + "\n");
+        sb.append(
+                "TIME:" + Build.TIME + "\n");
+        sb.append(
+                "builder类型：" + Build.TYPE + "\n");
+        sb.append(
+                "USER：" + Build.USER);
+        return sb.toString();
+    }
+
+
+    public static boolean isDeviceRooted() {
+        if (checkRootMethod1()) {
+            return true;
+        }
+        if (checkRootMethod2()) {
+            return true;
+        }
+        if (checkRootMethod3()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkRootMethod1() {
+        String buildTags = android.os.Build.TAGS;
+
+        if (buildTags != null && buildTags.contains("test-keys")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkRootMethod2() {
+        try {
+            File file = new File("/system/app/Superuser.apk");
+            if (file.exists()) {
+                return true;
+            }
+        } catch (Exception e) {
+        }
+
+        return false;
+    }
+
+    public static boolean checkRootMethod3() {
+        if (new ExecShell().executeCommand(ExecShell.SHELL_CMD.check_su_binary) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
+
+
+
